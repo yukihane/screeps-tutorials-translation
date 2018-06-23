0a1b1d05d1ceb342637f09bec18fee2cc28266c1
yukihane <yukihane.feather@gmail.com>
2018-06-23 15:37:25 +0900

    3-2

--- t03/t03-02.js
+++ t03/t03-02.js
@@ -29,12 +29,12 @@ function listener(details) {
       str = 
 `<section app-tutorial-content>
 <p>
-The second Controller level has <strong>5 extensions</strong> available for you to build.
-This number increases with each new level.
+第2のコントローラレベルに達するには<strong>5つの拡張</strong>を建設する必要があります。
+この数字は新しいレベルになるにつれ増えていきます。
 </p>
 <p>
-You can place extensions at any spot in your room, and a spawn can use them regardless of the distance.
-In this Tutorial we have already placed corresponding construction sites for your convenience.
+拡張を作る場所はルーム内のどこでも良く、スポーンは距離に関係なくそれらを使用できます。
+このチュートリアルでは便利なようにあらかじめ付随する構造物を設定しています。
 </p>
 <div class='tutorial-controls'>
 <md:button class='md-raised md-primary' ng:click='Tutorial.next()'>Next</md:button>
