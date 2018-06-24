5b3a45256b9d933b042070e57c36d0092ae19a32
yukihane <yukihane.feather@gmail.com>
2018-06-25 02:29:29 +0900

    4-4

--- t04/t04-04.js
+++ t04/t04-04.js
@@ -41,16 +41,16 @@ function listener(details) {
       str = 
 `<section app-tutorial-content>
 <p>
-Now let’s try to emulate a situation when one of our harvesters dies. You can now give the command
-<code>suicide</code> to the creep via the console or its properties panel on the right.
+harvesterが死んだ時のシチュエーションをエミュレートしてみましょう。
+コンソールあるいは右のプロパティパネルで<code>suicide</code>コマンドをクリープに対して実行できます。
 </p>
 <div class='objective'>
 <div class='fa fa-caret-right'></div>
-Make one of the harvesters suicide.
+harvesterを自殺させましょう。
 </div>
 <div class='objective'>
 <div class='fa fa-caret-down'></div>
-Documentation:
+ドキュメント:
 <ul>
 <li>
 <code><a app-nw-external-link href='http://docs.screeps.com/api/#Creep.suicide' target='_blank'>
